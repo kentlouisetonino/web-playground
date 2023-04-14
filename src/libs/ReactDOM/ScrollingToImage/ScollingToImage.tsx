fc34b2f8646f32878651f8e1a0c63e09ca3e6bd4
Kent Louise Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2023-04-15 00:26:52 +0800

    fix tomorrow the bug (#3)

---
+++ src/libs/ReactDOM/ScrollingToImage/ScollingToImage.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import { useRef } from 'react';
+import nodejsImage from '../../images/nodejs.png';
+import typescriptImage from '../../images/typescript.png';
+
+export default function ScrollingToImage() {
+  const reactjsImageRef = useRef<HTMLLIElement>(null);
+  const nodejsImageRef = useRef<HTMLLIElement>(null);
+  const typescriptImageRef = useRef<HTMLLIElement>(null);
+
+  function handleScrollToReactJS() {
+    if (reactjsImageRef.current) {
+      reactjsImageRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center',
+      });
+    }
+  }
+
+  function handleScrollToNodeJS() {
+    if (nodejsImageRef.current) {
+      nodejsImageRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center',
+      });
+    }
+  }
+
+  function handleScrollToTypeScript() {
+    if (typescriptImageRef.current) {
+      typescriptImageRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center',
+      });
+    }
+  }
+
+  return (
+    <div>
+      <nav>
+        <button onClick={handleScrollToReactJS}>ReactJS</button>
+        <button onClick={handleScrollToNodeJS}>NodeJS</button>
+        <button onClick={handleScrollToTypeScript}>TypeScript</button>
+      </nav>
+      <div>
+        <ul>
+          <li>
+            <Image src="/next.svg" alt="ReactJS" />
+          </li>
+          <li>
+            <Image src={nodejsImage} alt="NodeJS" />
+          </li>
+          <li>
+            <Image src={typescriptImage} alt="TypeScript" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
