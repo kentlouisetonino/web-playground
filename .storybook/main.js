3dc290b14136a10ad55d15c083c6ab8579752b76
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-06-10 19:13:18 +0800

    modify storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,17 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm',
-  ],
+  addons: ['@storybook/addon-essentials', 'storybook-addon-designs'],
   framework: {
     name: '@storybook/nextjs',
-    options: {},
+    options: {
+      options: {
+        nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      },
+    },
+  },
+  features: {
+    storyStoreV7: true,
   },
 };
--- src/libs/ReactDom/ScrollingToImage/ScrollingToImage.stories.tsx
+++
@@ -1,9 +0,0 @@
-import SScrollingToImage from './ScollingToImage';
-
-export default {
-  title: 'ReactDOM/Scrolling To Image',
-};
-
-export function ScrollingToImage() {
-  return <SScrollingToImage />;
-}
