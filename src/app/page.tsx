b763432db8f5cc57f2fcb0fafb9ccf89861b320b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-06-18 21:29:27 +0800

    modify home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">CRUEL WORLD</h1>;
+  return (
+    <h1 className="flex h-screen w-full items-center justify-center bg-black text-5xl font-bold text-white">
+      CRUEL WORLD
+    </h1>
+  );
 }
